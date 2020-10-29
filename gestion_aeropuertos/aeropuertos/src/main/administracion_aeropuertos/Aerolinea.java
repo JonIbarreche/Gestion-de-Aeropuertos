033ba4879c9dc774fa1b2cae6dc43f929206c4b6
JonIbarreche <55731819+JonIbarreche@users.noreply.github.com>
2020-10-29 18:47:28 +0100

    add Aerolinea and Vuelos

---
+++ gestion_aeropuertos/aeropuertos/src/main/administracion_aeropuertos/Aerolinea.java
@@ -0,0 +1,6 @@
+package gestion_aeropuertos.aeropuertos.src.main.administracion_aeropuertos;
+
+public class Aerolinea {
+    int codigo;
+    String nombre;
+}
---
+++ gestion_aeropuertos/aeropuertos/src/main/administracion_aeropuertos/Vuelos.java
@@ -0,0 +1,5 @@
+package gestion_aeropuertos.aeropuertos.src.main.administracion_aeropuertos;
+
+public class Vuelos {
+    int numero;
+}
