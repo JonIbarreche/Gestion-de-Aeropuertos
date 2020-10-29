379000f7612714d73b0b97ae4d3ac3faac2e7983
JonIbarreche <55731819+JonIbarreche@users.noreply.github.com>
2020-10-29 18:37:40 +0100

    Structure + App class created

---
+++ Gestion_Aeropuertos/aeropuertos/src/main/administracion_aeropuertos/App.java
@@ -0,0 +1,15 @@
+package gestion_aeropuertos.aeropuertos.src.main.administracion_aeropuertos;
+
+import javax.swing.SwingUtilities;
+
+public class App {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                //new login().setVisible(true);
+                // GUI Launcher
+            }
+        });
+    }
+}
