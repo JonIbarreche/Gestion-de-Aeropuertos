1ac298cd4c860f06229f741ee1e5eb2fd86ba458
ssc1999 <ssc1099.csmm@gmail.com>
2020-11-09 20:55:31 +0100

    VentanaLogin creada

--- Gestion_Aeropuertos/src/App.java
+++ Gestion_Aeropuertos/src/App.java
@@ -6,10 +6,10 @@ public class App {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-				Login frameLogin = new Login();
-				frameLogin.setVisible(true);
-				//Inicio frameInicio = new Inicio();
-				//frameInicio.setVisible(true);
+				//VentanaLogin frameLogin = new VentanaLogin();
+				//frameLogin.setVisible(true);
+				VentanaInicio frameInicio = new VentanaInicio();
+				frameInicio.setVisible(true);
 					//locations Locations = new ocations();
 				} catch (Exception e) {
 					e.printStackTrace();
