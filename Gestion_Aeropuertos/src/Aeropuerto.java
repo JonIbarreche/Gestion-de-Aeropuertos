23cf055517356fdcd5ec21f8ec9c58046d9f95d3
ssc1999 <ssc1099.csmm@gmail.com>
2020-11-18 10:42:16 +0100

    Clase Aeropuerto renovada

--- Gestion_Aeropuertos/src/Aeropuerto.java
+++ Gestion_Aeropuertos/src/Aeropuerto.java
@@ -1,57 +1,74 @@
+import java.util.Objects;
 
 public class Aeropuerto {
-    String nombre_aeropuerto;
-    String ciudad_aeropuerto;
-    String pais_aeropuerto;
+   
+    public static int contadorAeropuerto = 0;
+    protected int idAeropuerto;
+    protected String nombreAeropuerto;
+    protected String ciudadAeropuerto;
+    protected String paisAeropuerto;
 
-    public Aeropuerto(String nombre_aeropuerto, String ciudad_aeropuerto, String pais_aeropuerto) {
-        this.nombre_aeropuerto = nombre_aeropuerto;
-        this.ciudad_aeropuerto = ciudad_aeropuerto;
-        this.pais_aeropuerto = pais_aeropuerto;
+    public Aeropuerto(int idAeropuerto, String nombreAeropuerto, String ciudadAeropuerto, String paisAeropuerto) {
+        contadorAeropuerto++;
+        this.idAeropuerto = contadorAeropuerto;
+        this.nombreAeropuerto = nombreAeropuerto;
+        this.ciudadAeropuerto = ciudadAeropuerto;
+        this.paisAeropuerto = paisAeropuerto;
     }
 
     public Aeropuerto() {
-        this.nombre_aeropuerto = "";
-        this.ciudad_aeropuerto = "";
-        this.pais_aeropuerto = "";
+        contadorAeropuerto++;
+        this.idAeropuerto = contadorAeropuerto;
+        this.nombreAeropuerto = "";
+        this.ciudadAeropuerto = "";
+        this.paisAeropuerto = "";
     }
 
-    public String getNombre_aeropuerto() {
-        return this.nombre_aeropuerto;
+    public int getIdAeropuerto() {
+        return this.idAeropuerto;
     }
 
-    public void setNombre_aeropuerto(String nombre_aeropuerto) {
-        this.nombre_aeropuerto = nombre_aeropuerto;
+    public String getNombreAeropuerto() {
+        return this.nombreAeropuerto;
     }
 
-    public String getCiudad_aeropuerto() {
-        return this.ciudad_aeropuerto;
+    public void setNombreAeropuerto(String nombreAeropuerto) {
+        this.nombreAeropuerto = nombreAeropuerto;
     }
 
-    public void setCiudad_aeropuerto(String ciudad_aeropuerto) {
-        this.ciudad_aeropuerto = ciudad_aeropuerto;
+    public String getCiudadAeropuerto() {
+        return this.ciudadAeropuerto;
     }
 
-    public String getPais_aeropuerto() {
-        return this.pais_aeropuerto;
+    public void setCiudadAeropuerto(String ciudadAeropuerto) {
+        this.ciudadAeropuerto = ciudadAeropuerto;
     }
 
-    public void setPais_aeropuerto(String pais_aeropuerto) {
-        this.pais_aeropuerto = pais_aeropuerto;
+    public String getPaisAeropuerto() {
+        return this.paisAeropuerto;
     }
 
-    public Aeropuerto nombre_aeropuerto(String nombre_aeropuerto) {
-        this.nombre_aeropuerto = nombre_aeropuerto;
+    public void setPaisAeropuerto(String paisAeropuerto) {
+        this.paisAeropuerto = paisAeropuerto;
+    }
+
+    public Aeropuerto idAeropuerto(int idAeropuerto) {
+        this.idAeropuerto = idAeropuerto;
         return this;
     }
 
-    public Aeropuerto ciudad_aeropuerto(String ciudad_aeropuerto) {
-        this.ciudad_aeropuerto = ciudad_aeropuerto;
+    public Aeropuerto nombreAeropuerto(String nombreAeropuerto) {
+        this.nombreAeropuerto = nombreAeropuerto;
         return this;
     }
 
-    public Aeropuerto pais_aeropuerto(String pais_aeropuerto) {
-        this.pais_aeropuerto = pais_aeropuerto;
+    public Aeropuerto ciudadAeropuerto(String ciudadAeropuerto) {
+        this.ciudadAeropuerto = ciudadAeropuerto;
+        return this;
+    }
+
+    public Aeropuerto paisAeropuerto(String paisAeropuerto) {
+        this.paisAeropuerto = paisAeropuerto;
         return this;
     }
 
@@ -63,16 +80,21 @@ public class Aeropuerto {
             return false;
         }
         Aeropuerto aeropuerto = (Aeropuerto) o;
-        return nombre_aeropuerto == aeropuerto.nombre_aeropuerto && ciudad_aeropuerto == aeropuerto.ciudad_aeropuerto && pais_aeropuerto == aeropuerto.pais_aeropuerto;
+        return idAeropuerto == aeropuerto.idAeropuerto && Objects.equals(nombreAeropuerto, aeropuerto.nombreAeropuerto) && Objects.equals(ciudadAeropuerto, aeropuerto.ciudadAeropuerto) && Objects.equals(paisAeropuerto, aeropuerto.paisAeropuerto);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idAeropuerto, nombreAeropuerto, ciudadAeropuerto, paisAeropuerto);
     }
 
     @Override
     public String toString() {
         return "{" +
-            " nombre_aeropuerto='" + getNombre_aeropuerto() + "'" +
-            ", ciudad_aeropuerto='" + getCiudad_aeropuerto() + "'" +
-            ", pais_aeropuerto='" + getPais_aeropuerto() + "'" +
+            " idAeropuerto='" + getIdAeropuerto() + "'" +
+            ", nombreAeropuerto='" + getNombreAeropuerto() + "'" +
+            ", ciudadAeropuerto='" + getCiudadAeropuerto() + "'" +
+            ", paisAeropuerto='" + getPaisAeropuerto() + "'" +
             "}";
     }
-
 }
