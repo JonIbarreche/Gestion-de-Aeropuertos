a4f19779ef1828545ddb5790a75285e5213922e4
ssc1999 <ssc1099.csmm@gmail.com>
2021-01-08 17:34:39 +0100

    Controladores añadidos

---
+++ src/Controladores/Controlador.java
@@ -0,0 +1,18 @@
+package Controladores;
+
+import BDAPI.*;
+
+public class Controlador {
+
+    protected BDAPI bd; 
+
+    public Controlador(BDAPI bd) {
+        this.bd = bd;
+    }
+    
+    //Buscar Vuelos
+
+    //Cerrar Aplicacion
+    //
+
+}
